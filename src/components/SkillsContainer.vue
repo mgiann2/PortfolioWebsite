442ce243e176f92b7c34a6b15681bd69eb1aa984
Matteo Giannone <matteogiannone24@gmail.com>
2023-07-10 14:07:25 -0400

    Changed format of SkillsContainer

--- src/components/SkillsContainer.vue
+++ src/components/SkillsContainer.vue
@@ -2,9 +2,8 @@
 import FadeInContainer from './FadeInContainer.vue';
 import ThemeDiv from './ThemeDiv.vue';
 
-const techSkills = ["Java", "Python", "C", "C++", "C#", "Javascript", "HTML", "CSS", "Postgresql", "R",
+const skills = ["Java", "Python", "C", "C++", "C#", "Javascript", "HTML", "CSS", "Postgresql", "R",
                     "Django", "NodeJs", "ReactJs", "Vue", "JQuery", "Git", "Unix"];
-const softSkills = ["Problem-Solving", "Organization", "Time Management", "Communication"];
 </script>
 
 <template>
@@ -13,18 +12,7 @@ const softSkills = ["Problem-Solving", "Organization", "Time Management", "Commu
             <FadeInContainer delay="0s">
                 <h1>Skills</h1>
                 <div class="skill-grid">
-                    <div class="skill-col">
-                        <h2>Technical Skills</h2>
-                        <ul>
-                            <li v-for="skill in techSkills">{{ skill }}</li>
-                        </ul>
-                    </div>
-                    <div class="skill-col">
-                        <h2>Soft Skills</h2>
-                        <ul>
-                            <li v-for="skill in softSkills">{{ skill }}</li>
-                        </ul>
-                    </div>
+                    <span class="skill-span" v-for="skill in skills">{{ skill }}</span>
                 </div>
             </FadeInContainer>
         </div>
@@ -43,12 +31,16 @@ const softSkills = ["Problem-Solving", "Organization", "Time Management", "Commu
         justify-content: space-around;
     }
 
-    .skill-col {
-        
+    .skill-span {
+        width: 10rem;
+        margin: 0.3rem 0;
+        text-align: center;
+        font-size: large;
     }
 
     h1 {
         font-size: 3rem;
         font-weight: 200;
+        text-align: center;
     }
 </style>
