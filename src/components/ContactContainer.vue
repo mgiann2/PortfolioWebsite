2801275cab0ffc8cf15af2ca5a13d4bf32a71282
Matteo Giannone <matteogiannone24@gmail.com>
2023-07-13 16:36:08 -0400

    Modified email button

--- src/components/ContactContainer.vue
+++ src/components/ContactContainer.vue
@@ -11,9 +11,10 @@ import FadeInContainer from './FadeInContainer.vue';
             <div style="display: flex; justify-content: center;">
                 <abbr title="Get In Touch">
                     <a class="contact-btn" href="mailto:matteogiannone24@gmail.com" rel="noopener noreferrer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" style="margin-right: 0.5rem;" viewBox="0 0 16 16">
                             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                         </svg>
+                        Say Hi
                     </a>
                 </abbr>
             </div>
@@ -33,7 +34,7 @@ import FadeInContainer from './FadeInContainer.vue';
         align-items: center;
         justify-content: center;
         margin: 1rem 0;
-        padding: 0.75rem 1.75rem;
+        padding: 1rem 1rem;
         border-radius: 1rem;
         border: solid var(--black) 2px;
         text-decoration: none;
